e1504f55a3641f2516455a801665fba2c3c00de0
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-07-31 23:49:04 -0300

    #TASK Ajustando CORS para frontend da aplicação :: #RESOLVED

--- chatbot-api/src/main/java/com/chatbot/api/resource/QuestionCategoryResource.java
+++ chatbot-api/src/main/java/com/chatbot/api/resource/QuestionCategoryResource.java
@@ -1,28 +1,19 @@
 package com.chatbot.api.resource;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.chatbot.api.model.Article;
 import com.chatbot.api.model.QuestionCategory;
-import com.chatbot.api.repository.ArticleRepository;
 import com.chatbot.api.repository.QuestionCategoryRepository;
 
 @RestController
