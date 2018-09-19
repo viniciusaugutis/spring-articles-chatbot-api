da184208e0252234506a99cda1e4a8e024a87de6
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-09-19 02:39:50 -0300

    #TASK Ajustando CORS da API :: #RESOLVED

--- src/main/java/com/chatbot/api/cors/CorsFilter.java
+++ src/main/java/com/chatbot/api/cors/CorsFilter.java
@@ -36,7 +36,7 @@ public class CorsFilter implements Filter{
 		response.setHeader("Access-Control-Allow-Origin", chatbotApiProperty.getOriginPermited());
         response.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		if ("OPTIONS".equals(request.getMethod()) && "http://localhost:4200".equals(request.getHeader("Origin"))) {
+		if ("OPTIONS".equals(request.getMethod()) && chatbotApiProperty.getOriginPermited().equals(request.getHeader("Origin"))) {
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS, PATCH");
         	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
         	response.setHeader("Access-Control-Max-Age", "3600");
