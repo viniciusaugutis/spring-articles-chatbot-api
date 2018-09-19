0add8fa6cb8c165f976484f7fa0ba73187ff5ef3
Vinícius Augutis <vinicius_augutis@hotmail.com>
2018-09-19 00:40:15 -0300

    Removendo pasta desnecessaria

--- chatbot-api/src/main/resources/db/migration/V01__criando_categoria_de_artigo.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS article_category (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
-);
-
-INSERT INTO article_category(name) VALUES ('Arquitetura'), ('Decoração'),
-('Sustentabilidade'), ('Paisagismo');
